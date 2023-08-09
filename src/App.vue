7de5cc1d4a9c4d481cffd7567e5cac86a8893d6f
gaoxiaoxiang <1223149571@qq.com>
2023-08-09 16:16:21 +0800

    集成jsbrdige与原生交互

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1>Hello {{ title }} </h1>
+    <button class="btn" @click="onClick">call native方法</button>
+    <div class="content">
+      <label>params: {{ params }}</label>
+      <label>消息: {{ msg }}</label>
+      <label>平台: {{ platform }}</label>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: "world",
+      params: '',
+      msg: '',
+      platform : ''
+    }
+
+  }, 
+  beforeCreate() {
+      console.log('onCreate')
+      this.$bridge.registerHandler('callJS', (data, responseCallback)=> {
+        console.log("data: ", data)
+        let cbData = JSON.parse(data)
+        this.platform = cbData.platform
+        this.params = data
+        this.msg = '调用了callJS方法:'
+        let x = {
+          "platform":"js",
+          "type":2
+        }
+        responseCallback(x)
+      })
+    },
+  methods: {
+    
+    onClick() {
+      console.log(this)
+      let data = {
+        "action" : "go_goods_detail",
+        "params" : {
+          "name": "gerry"
+        }
+      }
+      this.$bridge.callhandler('callNative', data,
+        (cb) => {
+          let cbData = JSON.parse(cb)
+          let val = cbData.platform
+          console.log(cb)
+          this.params = cb
+          this.platform = val
+          this.title = 'gerry'
+          this.msg = '调用了callNative方法:'
+        })
+    }
   }
 }
 </script>
@@ -24,5 +71,28 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+}
+
+.btn {
+  height: 35px;
+  width: 150px;
+  background-color: dodgerblue;
+  color: white;
+  border: none;
+  font-size: 16px;
+  border-radius: 5px;
+}
+
+.content {
+  margin-top: 10px;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  font-size: 16px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//注册jsBridge 并挂载全局
+import Bridge from './src/jsbridge'
+
+Vue.prototype.$bridge = Bridge;
+
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+})//.$mount('#app') 
