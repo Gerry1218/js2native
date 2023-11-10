540187755b96e3365447adaa4235cddaf0ef8019
gaoxiaoxiang <1223149571@qq.com>
2023-11-10 14:09:53 +0800

    设置端口号 运行白屏。不显示ui，设置publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: 'localhost', // 项目运行的ip
+    port: 8082, // 项目运行的端口号
+  },
+  publicPath: './'
 })
